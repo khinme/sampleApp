c09d6a7706626a349789014e4c6ecb6b67644d32
demo2 <sendingcode123@gmail.com>
2022-06-02 10:48:18 +0630

    プロジェクト ファイルを追加します。

---
+++ sampleApp/sampleApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace sampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+    }
+}
